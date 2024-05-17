eb929a4e7a7469988c23c7c7381d60d1be4f9316
OpenCo <admin@openco.ca>
2024-05-17 09:05:55 +0000

    feat: create default.njk with meta tags and styles for auto layout of markdown

--- src/eleventy.config.js
+++ src/eleventy.config.js
@@ -6,10 +6,28 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const sectionizePlugin = require("./_plugins/eleventy-plugin-sectionize");
 
-module.exports = function(eleventyConfig) {
-	// Copy the contents of the `public` folder to wp-content to emulate WordPress behavior. 
-	eleventyConfig.addPassthroughCopy({
-		"./assets/": "/"
-	});
+module.exports = function (eleventyConfig) {
+  // Copy the contents of the `public` folder to wp-content to emulate WordPress behavior.
+  eleventyConfig.addPassthroughCopy({
+    "./assets/": "/",
+  });
+  eleventyConfig.addPlugin(sectionizePlugin);
+
+  // Set default layout for markdown files
+  eleventyConfig.addTemplateFormats("md");
+  eleventyConfig.addLayoutAlias("default", "default.njk");
+
+  eleventyConfig.addGlobalData("layout", "default");
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
 };
